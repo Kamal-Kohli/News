487d2a064f37ebfdf0490010bb43b8728db83d3c
Kamal Kohli <kamalkohli@Kamals-MacBook-Pro.local>
2024-02-23 19:17:38 +0100

    add news post to homepage

--- home/views.py
+++ home/views.py
@@ -1,30 +1,26 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
+
 import requests
 
 API_KEY = '2228671f48644d399e8cc37fb14015f6'
 
 # Create your views here.
+def fetch_news_data(page_size=10):
+    url = f'https://newsapi.org/v2/top-headlines?country=us&apiKey={API_KEY}'
+    response = requests.get(url)
+    data = response.json()
+
+    articles = data['articles']
+
+    return data
+
 class Index(TemplateView):
     template_name = 'home/index.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
-        country = self.request.GET.get('country')  # Accessing request using self.request
-        category = self.request.GET.get('category')  # Accessing request using self.request
-
-        if country:
-            url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey={API_KEY}'
-            response = requests.get(url)
-            data = response.json()
-            articles = data['articles']
-        elif category:
-            url = f'https://newsapi.org/v2/top-headlines?category={category}&apiKey={API_KEY}'
-            response = requests.get(url)
-            data = response.json()
-            articles = data['articles']
-        else:
-            articles = []  # Set articles to an empty list if neither country nor category is provided
-        
-        context['articles'] = articles
+        articles_data = fetch_news_data(page_size=20)  # Fetch 20 articles
+        context['articles'] = articles_data['articles']
+        context['total_articles'] = articles_data['totalResults']
         return context

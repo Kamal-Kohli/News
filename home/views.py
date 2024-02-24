09a1a514619fd247d6d4d9289c2023db116dbf46
Kamal Kohli <kamalkohli@Kamals-MacBook-Pro.local>
2024-02-24 13:21:57 +0100

    add country and cetagories in Navbar

--- home/views.py
+++ home/views.py
@@ -1,17 +1,23 @@
 from django.views.generic import TemplateView
-from django.shortcuts import render
-
 import requests
 
 API_KEY = '2228671f48644d399e8cc37fb14015f6'
 
 # Create your views here.
-def fetch_news_data(page_size=10):
-    url = f'https://newsapi.org/v2/top-headlines?country=us&apiKey={API_KEY}'
-    response = requests.get(url)
-    data = response.json()
+def fetch_news_data(request, page_size=10):
+    country = request.GET.get('country') 
+    category = request.GET.get('category')
 
-    articles = data['articles']
+    if country:
+        url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey={API_KEY}'
+        response = requests.get(url)
+        data = response.json()
+        articles = data['articles']
+    else:
+        url = f'https://newsapi.org/v2/top-headlines?category={category}&apiKey={API_KEY}'
+        response = requests.get(url)
+        data = response.json()
+        articles = data['articles']
 
     return data
 
@@ -20,7 +26,7 @@ class Index(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        articles_data = fetch_news_data(page_size=20)  # Fetch 20 articles
+        articles_data = fetch_news_data(self.request, page_size=20)    
         context['articles'] = articles_data['articles']
         context['total_articles'] = articles_data['totalResults']
         return context

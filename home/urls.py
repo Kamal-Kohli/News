ee1a351fb06b1091ab217c4298546fefd63df2e5
Kamal Kohli <kamalkohli@Kamals-MacBook-Pro.local>
2024-02-24 21:01:28 +0100

    Add image instead of Unavailable in news articles

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .views import Index
+from . import views
 
 
 
 urlpatterns = [
     path('', Index.as_view(), name='home'),
-    
 ] 
--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
 import requests
 
 API_KEY = '2228671f48644d399e8cc37fb14015f6'
